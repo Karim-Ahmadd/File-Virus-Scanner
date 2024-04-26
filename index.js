307035e348fabc798e144d46cdf93dc191d3cd71
Karim-Ahmadd <karim.ahmad2404@gmail.com>
2024-04-27 00:52:57 +0300

    setup file form and multer in server

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import axios from "axios";
+import multer from "multer";
+import ejs from "ejs";
+
+const app = express();
+const port =3000;
+
+const upload = multer({dest: "uploads/"});
+
+app.use(express.static("public"));
+
+
+app.get("/", function(req,res){
+    res.render("index.ejs");
+});
+
+
+
+app.post("/upload", upload.single("file"), function(req,res){
+    console.log(req.file);
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, function(){
+    console.log("the server is listening to port "+ port);
+})
